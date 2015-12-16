84db34162ba550362c92a6a53c65cb4e14ff5c23
Jim Terman <jim@termanweb.net>
2015-12-16 15:24:37 -0800

    Final doxygen comments

--- milo_ncurses.cpp
+++ milo_ncurses.cpp
@@ -5,9 +5,14 @@
 
 using namespace std;
 
+/**
+ * Class derived from ABC Graphics to allow milo to draw to a ncurses ascii screen.
+ */
 class CursesGraphics : public Graphics
 {
 public:
+	/** @name Constructor and Virtual Destructor */
+	//@{
 	CursesGraphics() { 
 		if (!init) {
 			initscr(); raw(); noecho(); 
@@ -28,16 +33,69 @@ public:
 		}
 	}
 	~CursesGraphics() { endwin(); }
+	//@}
 
+	/** @name Virtual Public Member Functions */
+	//@{
+
+	/**
+	 * Get height of text in lines.
+	 * @return All ASCII characters are one line in height.
+	 */
 	int getTextHeight() { return 1; }
+
+	/**
+	 * Get length of string in characters.
+	 * @return Length of string in characters.
+	 */
 	int getTextLength(const std::string& s) { return s.length(); }
+
+	/**
+	 * Length of character in ASCII is 1.
+	 * @return Return one.
+	 */
 	int getCharLength(char) { return 1; }
+
+	/**
+	 * Get width of a parenthesis in characters which is always 1.
+	 * @return Return one.
+	 */
 	int getParenthesisWidth(int) { return 1; }
+
+	/**
+	 * Get height of line in a division node which is always 1.
+	 * @return Return one.
+	 */
 	int getDivideLineHeight() { return 1; }
+
+	/**
+	 * Get height of differential in lines of text.
+	 * @param c Variable name of differential.
+	 * @return Height of differential in lines of text.
+	 */
 	int getDifferentialHeight(char) { return 3; }
+
+	/**
+	 * Get width of differential in lines of text.
+	 * @param c Variable name of differential.
+	 * @return Width of differential in lines of text.
+	 */
 	int getDifferentialWidth(char)  { return 2; }
+
+	/**
+	 * Get vertical offset of differential.
+	 * @param c Variable name of differential.
+	 * @return Vertical offset of differential in lines of text.
+	 */
 	int getDifferentialBase(char)   { return 1; }
 
+	/**
+	 * Draw a string at x,y with a color.
+	 * @param x0 Horizontal origin of line.
+	 * @param y0 Vertical origin of line.
+	 * @param c  Character to be drawn at x0,y0.
+	 * @param color Color of line.
+	 */
 	void at(int x, int y, int c, Color color = BLACK) {
 		if (c == 'P') c = ACS_PI;
 		if (m_select.inside(x, y)) c |= A_REVERSE;
@@ -46,6 +104,13 @@ public:
 		if (color != BLACK && m_has_colors) attroff(COLOR_PAIR(color));
 	}
 
+	/**
+	 * Draw a string at x,y with a color.
+	 * @param x0 Horizontal origin of line.
+	 * @param y0 Vertical origin of line.
+	 * @param s  String to be drawn at x0,y0.
+	 * @param color Color of line.
+	 */
 	void at(int x, int y, const string& s, Color color = BLACK) {
 		if (color != BLACK && m_has_colors) attron(COLOR_PAIR(color));
 		if (m_select.inside(x, y)) attron(A_REVERSE);
@@ -56,8 +121,18 @@ public:
 		if (m_select.inside(x, y)) attroff(A_REVERSE);
 	}
 
+	/**
+	 * Flush all characters to the ncurses screen.
+	 */
 	void out() { refresh(); }
 
+	/**
+	 * Set size of this graphics window (origin is always 0,0).
+	 * @param x Horizontal size of graphics window.
+	 * @param y Vertical size of graphics window.
+	 * @param x0 Not used.
+	 * @param y0 Not used.
+	 */
 	void set(int x, int y, int, int) { 
 		int row, col;
 		getmaxyx(stdscr, row, col);
@@ -65,23 +140,63 @@ public:
 		clear();
 	}
 
+	/**
+	 * Draw a horizontal line starting at x0,y0 length x_size.
+	 * @param x_size Horizontal size of both line.
+	 * @param x0 Horizontal origin of line.
+	 * @param y0 Vertical origin of line.
+	 */
 	void horiz_line(int x_size, int x0, int y0) { 
 		for (int i = 0; i < x_size; ++i) at(x0 + i, y0, ACS_HLINE);
 	}
 
+	/**
+	 * Draw differential of width x0 and height y0 with char variable name.
+	 * @param x0 Horizontal origin of differential.
+	 * @param y0 Vertical origin of differential.
+	 * @param variable Name of variable of differential.
+	 */
 	void differential(int x0, int y0, char variable);
 
+	/**
+	 * Draw pair of parenthesis around a node of size x_size, y_size and origin x0,y0.
+	 * @param x_size Horizontal size of both parenthesis.
+	 * @param y_size Vertical size of parenthesis.
+	 * @param x0 Horizontal origin of parenthesis.
+	 * @param y0 Vertical origin of parenthesis.
+	 */
 	void parenthesis(int x_size, int y_size, int x0, int y0);
 
+	/**
+	 * Select the area of size x,y at origin x0,y0.
+	 * @param x Horizontal size of both selection area.
+	 * @param y Vertical size of selection area.
+	 * @param x0 Horizontal origin of selection area.
+	 * @param y0 Vertical origin of selection area.
+	 */
 	void setSelect(int x, int y, int x0, int y0) { 
 		Graphics::setSelect(x, y, x0, y0);
 		for (int j = 0; j < y; ++j) mvchgat(m_yOrig + y0 + j, m_xOrig + x0, x, A_REVERSE, 0, NULL);
 	}
+	//@}
 
+	/**
+	 * Return character drawn at screen coordinates.
+	 * @param x Horizontal coordinate.
+	 * @param y Vertical coordinate.
+	 * @return Character at x,y coordinate.
+	 */
 	unsigned int ins(int x, int y) {
 		return mvinch(y + m_yOrig, x + m_xOrig);
 	}
 
+	/**
+	 * Return character from key press. Put up a blinking cursor at given coordinates.
+	 * Erase cursor after key press.
+	 * @param y Vertical coordinate.
+	 * @param x Horizontal coordinate.
+	 * @return Character from keyboard.
+	 */
 	unsigned int getChar(int y, int x) {
 		curs_set(2);
 		mvaddch(y + m_yOrig, x + m_xOrig, ' '); 
@@ -94,6 +209,10 @@ public:
 		return ch;
 	}
 
+	/**
+	 * Static fucntion that return singleton CursesGraphics object attached to screen
+	 * @return CursesGraphics singleton.
+	 */
 	static CursesGraphics& getInstance() {
 		static CursesGraphics draw;
 		
@@ -101,8 +220,8 @@ public:
 	}
 
 private:
-	static bool init;
-	static bool m_has_colors;
+	static bool init;          ///< Singleton has been initialized.
+	static bool m_has_colors;  ///< Singleton flag is screen has colors.
 };
 
 bool CursesGraphics::init = false;
@@ -136,7 +255,12 @@ void CursesGraphics::differential(int x0, int y0, char variable)
 	at(x0+1, y0+2, variable);
 }
 
-
+/**
+ * Main routine for milo ncurses command line app.
+ * @param argc Number of arguments. Only first is used for equation xml file.
+ * @param argv Array of argugments.
+ * @return Exit code.
+ */
 int main(int argc, char* argv[])
 {
 	LOG_TRACE_CLEAR();
--- milo_test.cpp
+++ milo_test.cpp
@@ -5,41 +5,141 @@
 
 using namespace std;
 
+/**
+ * Class derived from ABC graphics so Equation can draw to a 2D character array.
+ */
 class AsciiGraphics : public Graphics
 {
 public:
+	/** @name Constructor and Virtual Destructor */
+	//@{
 	AsciiGraphics(ostream& os) : Graphics(), m_os(os) {}
 
 	~AsciiGraphics() { }
+	//@}
 
+	/** @name Virtual Public Member Functions */
+	//@{
+
+	/**
+	 * Get height of text in lines.
+	 * @return All ASCII characters are one line in height.
+	 */
 	int getTextHeight() { return 1; }
+
+	/**
+	 * Get length of string in characters.
+	 * @return Length of string in characters.
+	 */
 	int getTextLength(const std::string& s) { return s.length(); }
+
+	/**
+	 * Length of character in ASCII is 1.
+	 * @return Return one.
+	 */
 	int getCharLength(char) { return 1; }
+
+	/**
+	 * Get width of a parenthesis in characters which is always 1.
+	 * @return Return one.
+	 */
 	int getParenthesisWidth(int) { return 1; }
+
+	/**
+	 * Get height of line in a division node which is always 1.
+	 * @return Return one.
+	 */
 	int getDivideLineHeight() { return 1; }
+
+	/**
+	 * Get height of differential in lines of text.
+	 * @param c Variable name of differential.
+	 * @return Height of differential in lines of text.
+	 */
 	int getDifferentialHeight(char) { return 3; }
+
+	/**
+	 * Get width of differential in lines of text.
+	 * @param c Variable name of differential.
+	 * @return Width of differential in lines of text.
+	 */
 	int getDifferentialWidth(char)  { return 2; }
+
+	/**
+	 * Get vertical offset of differential.
+	 * @param c Variable name of differential.
+	 * @return Vertical offset of differential in lines of text.
+	 */
 	int getDifferentialBase(char)   { return 1; }
 
+	/**
+	 * Draw differential of width x0 and height y0 with char variable name.
+	 * @param x0 Horizontal origin of differential.
+	 * @param y0 Vertical origin of differential.
+	 * @param variable Name of variable of differential.
+	 */
 	void differential(int x0, int y0, char variable);
+
+	/**
+	 * Draw pair of parenthesis around a node of size x_size, y_size and origin x0,y0.
+	 * @param x_size Horizontal size of both parenthesis.
+	 * @param y_size Vertical size of parenthesis.
+	 * @param x0 Horizontal origin of parenthesis.
+	 * @param y0 Vertical origin of parenthesis.
+	 */
 	void parenthesis(int x_size, int y_size, int x0, int y0);
+
+	/**
+	 * Draw a horizontal line starting at x0,y0 length x_size.
+	 * @param x_size Horizontal size of both line.
+	 * @param x0 Horizontal origin of line.
+	 * @param y0 Vertical origin of line.
+	 */
 	void horiz_line(int x_size, int x0, int y0) { for (int i = 0; i < x_size; ++i) at(x0 + i, y0, '-'); }
+
+	/**
+	 * Draw a string at x,y with a color.
+	 * @param x0 Horizontal origin of line.
+	 * @param y0 Vertical origin of line.
+	 * @param c  Character to be drawn at x0,y0.
+	 * @param color Color of line.
+	 */
 	void at(int x, int y, int c, Color color = BLACK) { m_field[y][x] = c; m_colors[y][x] = color; }
+
+	/**
+	 * Draw a string at x,y with a color.
+	 * @param x0 Horizontal origin of line.
+	 * @param y0 Vertical origin of line.
+	 * @param s  String to be drawn at x0,y0.
+	 * @param color Color of line.
+	 */
 	void at(int x, int y, const string& s, Color color = BLACK);
 
+
+	/**
+	 * Flush all characters to the text array.
+	 */
 	void out();
 
-	virtual void set(int x, int y, int x0 = 0, int y0 = 0) { 
-		Graphics::set(x, y, x0, y0);
+	/**
+	 * Set size of the text array (origin is always 0,0).
+	 * @param x Horizontal size of text array.
+	 * @param y Vertical size of text array.
+	 * @param x0 Not used.
+	 * @param y0 Not used.
+	 */
+	virtual void set(int x, int y, int, int) { 
+		Graphics::set(x, y, 0, 0);
 		for (int i = 0; i < y; ++i) {
 			m_field.emplace_back(x, ' ');
 			m_colors.emplace_back(x, Color::BLACK);
 		}
 	}
+	//@}
 private:
-	vector<string> m_field;
-	vector< vector<Color> > m_colors;
-	ostream& m_os;
+	vector<string> m_field;           ///< 2D text array.
+	vector< vector<Color> > m_colors; ///< 2D color array.
+	ostream& m_os;                    ///< Stream to ouput text array.
 };
 
 void AsciiGraphics::at(int x, int y, const string& s, Color color)
@@ -88,14 +188,24 @@ void AsciiGraphics::differential(int x0, int y0, char variable)
 	m_field[y0+2][x0+ 1] = variable;
 }
 
+/** Global Equation object.
+ */
 static Equation eqn("?");
 
+/**
+ * Load parsed equation string into global equation object.
+ * @param eqn_str String with text version of equation.
+ */
 static void parse(const string& eqn_str)
 {
 	Equation new_eqn(eqn_str);
 	eqn = new_eqn;
 }
 
+/**
+ * Load filename with xml version of equation
+ * @param fname Name of file with xml
+ */
 static void xml_in(const string& fname)
 {
 	ifstream in(fname);
@@ -103,6 +213,8 @@ static void xml_in(const string& fname)
 	eqn = new_eqn;
 }
 
+/** Output current equation in xml to standard output.
+ */
 static void xml_out(const string&)
 {
 	string xml;
@@ -110,6 +222,8 @@ static void xml_out(const string&)
 	cout << xml << endl;
 }
 
+/** Test output of current equation.
+ */
 static void test(const string&)
 {
 	AsciiGraphics gc(cout);
@@ -131,11 +245,15 @@ static void test(const string&)
 	cout << "---------" << endl;
 }
 
+/** Output current equation in ascii to standard output.
+ */
 static void eqn_out(const string&)
 {
 	cout << eqn.toString() << endl;
 }
 
+/** Output current equation as ascii art to standard output.
+ */
 static void art(const string&)
 {
 	AsciiGraphics gc(cout);
@@ -143,20 +261,30 @@ static void art(const string&)
 	gc.out();
 }
 
+/** Normalize current equation.
+ */
 static void normalize(const string&)
 {
 	eqn.normalize();
 }
 
+/** Simplify current equation.
+ */
 static void simplify(const string&)
 {
 	eqn.simplify();
 }
 
+/** Output help to standard output.
+ */
 static void help(const string&);
 
+/** Pointer to function from command line argument.
+ */
 typedef void (*func_ptr)(const string& s);
 
+/** Associate command with an internal function. ':' means it takes argument.
+ */
 const map<string, func_ptr> test_funcs = {
 	{ "parse:",    parse     },
 	{ "xml:",      xml_in    },
@@ -185,6 +313,14 @@ static void help(const string&)
 	}
 }
 
+/**
+ * Command line milo test main function.
+ * Takes name of test for argument plus argument passed to text if needed.
+ * More than one test command maybe passed on command line for serial tests.
+ * @param argc Number of arguments.
+ * @param argv Array of argugments.
+ * @return Exit code.
+ */
 int main(int argc, char* argv[])
 {
 	int i = 1;
